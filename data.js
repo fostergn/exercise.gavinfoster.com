fa7c177a685eb15b7bc2310767162ea938fd4e95
Jerry <jerrydwalters@gmail.com>
2017-03-18 17:48:53 -0400

    add mock data and test endpoints for contacts

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        "id": 1,
+        "name": "Jerry Walters",
+        "email": "jerrywalters@gmail.com"
+    },
+    {
+        "id": 2,
+        "name": "Gavin Foster",
+        "email": "gavinfoster@gmail.com"
+    },
+    {
+        "id": 3,
+        "name": "Emily Plemmons",
+        "email": "emilyplemmons@gmail.com"
+    },
+    {
+        "id": 4,
+        "name": "Kyle Marlowe",
+        "email": "kylemarlowe@gmail.com"
+    },
+]
+
+module.exports = data;
--- server.js
+++ server.js
@@ -1,12 +1,35 @@
+'use strict';
+
+const contacts = require('./data.js');
+
 // dependencies
 const express = require('express'),
   app = express(),
+  bodyParser = require('body-parser'),
   port = process.env.PORT || 8080;
 
 // configure app
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // set routes
 app.use(require('./app/routes'));
+
+app.get('/api/contacts', (req, res) => {
+  if(!contacts) {
+    res.status(404).json({ message: 'No contacts found!'})
+  }
+  res.json(contacts);
+})
+
+app.get('/api/contacts/:id', (req, res) => {
+  const reqId = req.params.id;
+
+  let contact = contacts.filter(contact => {
+    return contact.id == reqId;
+  });
+
+  res.json(contact[0])
+})
 
 // start server
 app.listen(port, () => {
